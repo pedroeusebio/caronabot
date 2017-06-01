401009b8bae0331fc0aff6ec547127d719d6527e
Pedro Eusébio <pedroeusebio@poli.ufrj.br>
2017-05-31 22:25:22 -0300

    adding multiple response: recursive to ensure response's order;

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import rp from 'request-promise';
+import R from 'ramda';
+const url = `https://graph.facebook.com/v2.9/me/messages?access_token=${process.env.PAGE_TOKEN}`;
+
 
 const headers = {
   'Content-Type': 'application/json'
 };
 
 
-async function messengerResponse(content, recipient_id, url) {
+async function messengerResponse(content, recipient_id) {
   const options = {
     url : url,
     method : 'POST',
@@ -18,17 +21,38 @@ async function messengerResponse(content, recipient_id, url) {
     }
   };
   return await rp(options)
-    .then((response) => response)
-    .catch((err) => err);
+    .then(response => response)
+    .catch(err => err);
+}
+
+async function multipleResponse(arrayContent, recipient_id) {
+  if(arrayContent.length > 1) {
+    messengerResponse(arrayContent[0], recipient_id).then(res => {
+      multipleResponse(R.slice(1, Infinity, arrayContent), recipient_id);
+    });
+  } else {
+    messengerResponse(arrayContent[0], recipient_id).then(res => res);
+  }
 }
 
 async function facebookSearch(recipient_id) {
+  const urlUser = `https://graph.facebook.com/v2.9/${recipient_id}?access_token=${process.env.PAGE_TOKEN}`;
+  const options = {
+    url : urlUser,
+    method : 'GET',
+    headers : headers
+  };
+
+  return await rp(options)
+    .then(response => JSON.parse(response))
+    .catch(err => err);
 }
 
 const api = {
-  response: messengerResponse
+  response: messengerResponse,
+  multipleResponse: multipleResponse,
+  get_user: facebookSearch
 };
 
 
-module.exports = api;
-
+export default api;
